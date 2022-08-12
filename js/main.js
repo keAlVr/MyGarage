28f587d123358d2c7e7e7dee16f20aeb566dffb0
[kevin renard] <[renardk.webdev@gmail.com]>
2022-08-12 18:15:25 +0200

    Project start

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.header__menuToggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.style.height = "100vh"
+})
