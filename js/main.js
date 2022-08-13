631509078d8c1d2eccecf0eabde56fa06ee94255
[kevin renard] <[renardk.webdev@gmail.com]>
2022-08-13 21:19:33 +0200

    Better start

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.header__menuToggle');
-const nav = document.querySelector('nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.style.height = "100vh"
-})
